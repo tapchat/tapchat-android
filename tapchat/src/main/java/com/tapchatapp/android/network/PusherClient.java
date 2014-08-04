427d09131dd8ef82f26f8797ecc791e15fec98c8
Eric Butler <eric@codebutler.com>
2014-08-03 22:22:06 -0700

    Clean up PusherClient slightly.

--- tapchat/src/main/java/com/tapchatapp/android/network/PusherClient.java
+++ tapchat/src/main/java/com/tapchatapp/android/network/PusherClient.java
@@ -21,6 +21,7 @@ import android.os.AsyncTask;
 import android.util.Base64;
 import android.util.Log;
 
+import com.crashlytics.android.Crashlytics;
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 import com.tapchatapp.android.app.TapchatApp;
 import com.tapchatapp.android.client.TapchatPushAPI;
@@ -37,6 +38,8 @@ import retrofit.client.Response;
 public class PusherClient {
     private static final String TAG = "TapChatPusherClient";
 
+    private final boolean mDebug;
+
     private String mPushId;
     private byte[] mPushKey;
     private String mRegId;
@@ -48,82 +51,89 @@ public class PusherClient {
     public PusherClient(String id, String key, String gcmRegId) {
         TapchatApp.get().inject(this);
 
+        mDebug = TapchatApp.get().getPreferences().getBoolean(TapchatApp.PREF_DEBUG, false);
+
         setTapchatPushInfo(id, key);
         mRegId = gcmRegId;
     }
 
     public void setTapchatPushInfo(String id, String key) {
-        Log.d(TAG, String.format("setTapchatPushInfo %s %s", id, key));
+        if (mDebug) Log.d(TAG, String.format("setTapchatPushInfo %s %s", id, key));
         mPushId  = id;
         mPushKey = (key != null) ? Base64.decode(key, Base64.URL_SAFE | Base64.NO_WRAP) : null;
         submitIfReady();
     }
 
     public void start() {
-        Log.d(TAG, "start()");
+        if (mDebug) Log.d(TAG, "start()");
         new AsyncTask<Void, Void, String>() {
-            @Override
-            protected String doInBackground(Void... unused) {
+            @Override protected String doInBackground(Void... unused) {
                 try {
                     return mGcm.register(TapchatApp.GCM_SENDER_ID);
                 } catch (IOException e) {
-                    Log.e(TAG, "GCM registration failed", e);
+                    if (mDebug) Log.e(TAG, "GCM registration failed", e);
                     return null;
                 }
             }
 
-            @Override
-            protected void onPostExecute(String regId) {
-                Log.d(TAG, String.format("Got GCM regId: %s", regId));
+            @Override protected void onPostExecute(String regId) {
+                if (mDebug) Log.d(TAG, String.format("Got GCM regId: %s", regId));
 
-                SharedPreferences.Editor prefs = TapchatApp.get().getPreferences().edit();
+                SharedPreferences prefs = TapchatApp.get().getPreferences();
+                SharedPreferences.Editor editor = prefs.edit();
 
                 if (regId == null) {
-                    prefs.remove(TapchatApp.PREF_GCM_REG_ID);
-                    prefs.apply();
+                    editor.remove(TapchatApp.PREF_GCM_REG_ID);
+                    editor.apply();
                     return;
                 }
 
-                prefs.putString(TapchatApp.PREF_GCM_REG_ID, regId);
-                prefs.apply();
+                if (regId.equals(prefs.getString(TapchatApp.PREF_GCM_REG_ID, null))) {
+                    if (mDebug) Log.d(TAG, "GCM ID didn't change.");
+                    return;
+                }
 
-//                if (!regId.equals(mRegId)) {
-                    mRegId = regId;
-                    submitIfReady();
-//                }
+                editor.putString(TapchatApp.PREF_GCM_REG_ID, regId);
+                editor.apply();
+
+                mRegId = regId;
+                submitIfReady();
             }
         }.execute();
     }
 
     public void unregister() {
-        Log.d(TAG, "unregister()");
+        new Thread(new Runnable() {
+            @Override public void run() {
+                try {
+                    mGcm.unregister();
 
-//        try {
-//  FIXME: main thread           mGcm.unregister();
-//         } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
+                    // FIXME: If mPushId is null...should we wait for that?
+                    final String oldPushId = mPushId;
+                    final String oldRegId  = mRegId;
 
-        // FIXME: If mPushId is null...should we wait for that?
-        final String oldPushId = mPushId;
-        final String oldRegId  = mRegId;
+                    mPushId  = null;
+                    mPushKey = null;
+                    mRegId   = null;
 
-        mPushId  = null;
-        mPushKey = null;
-        mRegId   = null;
+                    mRequestSent = false;
 
-        mRequestSent = false;
-
-        if (oldPushId != null && oldRegId != null) {
-            mPushApi.unregister(new TapchatPushAPI.Body(oldPushId, oldRegId), new Callback<TapchatPushAPI.Result>() {
-                @Override public void success(TapchatPushAPI.Result result, Response response) {
-                    Log.d(TAG, "Unregistered id: " + oldPushId + " reg: " + oldRegId);
+                    if (oldPushId != null && oldRegId != null) {
+                        mPushApi.unregister(new TapchatPushAPI.Body(oldPushId, oldRegId), new Callback<TapchatPushAPI.Result>() {
+                            @Override public void success(TapchatPushAPI.Result result, Response response) {
+                                if (mDebug) Log.d(TAG, "Unregistered id: " + oldPushId + " reg: " + oldRegId);
+                            }
+                            @Override public void failure(RetrofitError e) {
+                                if (mDebug) Log.e(TAG, "Failed to unregister id: " + oldPushId + " reg: " + oldRegId, e);
+                            }
+                        });
+                    }
+                } catch (Exception ex) {
+                    Crashlytics.logException(ex);
+                    Log.e(TAG, "Failed to unregister", ex);
                 }
-                @Override public void failure(RetrofitError e) {
-                    Log.e(TAG, "Failed to unregister id: " + oldPushId + " reg: " + oldRegId, e);
-                }
-            });
-        }
+            }
+        }).start();
     }
 
     public byte[] getPushKey() {
@@ -131,8 +141,10 @@ public class PusherClient {
     }
 
     private synchronized void submitIfReady() {
-        Log.d(TAG, String.format("submitIfReady() sent: %s, id: %s, key: %s, regId: %s",
-            mRequestSent, mPushId, Arrays.toString(mPushKey), mRegId));
+        if (mDebug) {
+            Log.d(TAG, String.format("submitIfReady() sent: %s, id: %s, key: %s, regId: %s",
+                    mRequestSent, mPushId, Arrays.toString(mPushKey), mRegId));
+        }
 
         if (mPushId == null || mPushKey == null || mRegId == null) {
             // Not yet ready
@@ -147,10 +159,10 @@ public class PusherClient {
 
         mPushApi.register(new TapchatPushAPI.Body(mPushId, mRegId), new Callback<TapchatPushAPI.Result>() {
             @Override public void success(TapchatPushAPI.Result result, Response response) {
-                Log.d(TAG, "Registered id: " + mPushId + " reg: " + mRegId);
+                if (mDebug) Log.d(TAG, "Registered id: " + mPushId + " reg: " + mRegId);
             }
             @Override public void failure(RetrofitError e) {
-                Log.e(TAG, "Failed to register id: " + mPushId + " reg: " + mRegId, e);
+                if (mDebug) Log.e(TAG, "Failed to register id: " + mPushId + " reg: " + mRegId, e);
                 mRequestSent = false;
             }
         });
